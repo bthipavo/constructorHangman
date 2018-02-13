8f029e87735883f25a9a9db4720d19d82670fb9d
bthipavo <bthipavo@gmail.com>
2018-02-12 23:27:50 -0800

    initial files added

---
+++ letter.js
@@ -0,0 +1,21 @@
+
+function Letter(letter, guess) {
+	this.letter = letter
+	this.guess = guess
+	
+// A function that returns the underlying character 
+// if the letter has been guessed, or a placeholder 
+// (like an underscore) if the letter has not been guessed
+
+	this.wordGuess = function() {
+
+	}
+
+// A function that takes a character as an argument and checks 
+// it against the underlying character, updating the stored
+ // boolean value to true if it was guessed correctly
+
+	this.compareGuess = function() {
+
+	}
+}
